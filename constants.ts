2282a2cd4e510611cc09d9de1eadda648bb1b0e5
Henit Chobisa <chobisa.henit@gmail.com>
2023-01-01 23:08:58 +0530

    feat: Constants Added

---
+++ constants.ts
@@ -0,0 +1,10 @@
+export enum workspaceStatus {
+    workspace_Inactive = "stopped",
+    workspace_active = "running"
+}
+
+export enum statusColors {
+    running = "#84cc18",
+    loading = "#e09f51",
+    stopped = "#a8a29e",
+}

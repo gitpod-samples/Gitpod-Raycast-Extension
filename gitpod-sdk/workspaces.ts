588884408ff9fdea006449c816d37fa2e52eeabc
Palanikannan1437 <palanikannan.m2020@vitstudent.ac.in>
2023-01-02 01:00:50 +0530

    modified sdk and sample data

--- gitpod-sdk/workspaces.ts
+++ gitpod-sdk/workspaces.ts
@@ -10,89 +10,101 @@ export async function listWorkspaces(Pagesize: number): Promise<IWorkspace[]> {
         context: {
           branch: "my-branch",
           commit: "f5d4eb4cd3859a760ac613598e840b94e8094649",
-          type: "small",
-          date: new Date(),
+          type: "Standard",
+          date: new Date(1628618888939),
+          source : "Palanikannan1437/RC4Community"
         },
         status: "running",
+        isGitPodified: true, // will be determinded on the basis of if .gitpod.yml is present in the repository
       },
       {
-        workspaceId: "2",
+        workspaceId: "https://gitpod.io/start/#raycast-extensions-lfi6vqr3348",
         ownerId: "string",
         projectId: "Gitpod-website",
         context: {
           branch: "my-branch-3",
           commit: "f5d4eb4cd3859a760ac613598e840b94e8094649",
-          type: "small",
+          type: "Large",
           date: new Date(),
+          source : "Palanikannan1437/Gitpod-website"
         },
         status: "stopped",
+        isGitPodified: true,
       },
       {
-        workspaceId: "3",
+        workspaceId: "kapadianait-rc4communit-0b4986ftvc3",
         ownerId: "string",
         projectId: "Gitpod",
         context: {
           branch: "my-branch-3",
           commit: "f5d4eb4cd3859a760ac613598e840b94e8094649",
-          type: "small",
+          type: "Standard",
           date: new Date(),
+          source : "Palanikannan1437/kapadinait"
         },
-        status: "running",
+        status: "progressing",
+        isGitPodified: false,
       },
       {
-        workspaceId: "4",
+        workspaceId: "gitpodio-templatek3s-sbrz83lp885",
         ownerId: "string",
         projectId: "Gitpod-4",
         context: {
           branch: "my-branch-4",
           commit: "f5d4eb4cd3859a760ac613598e840b94e8094649",
-          type: "large",
+          type: "Large",
           date: new Date(),
+          source : "Palanikannan1437/gitpodio-template"
         },
         status: "running",
+        isGitPodified: true,
       },
       {
-        workspaceId: "5",
+        workspaceId: "palanikanna-incubatorde-qymiz93y5la",
         ownerId: "string",
         projectId: "Gitpod-5",
         context: {
           branch: "my-branch",
           commit: "f5d4eb4cd3859a760ac613598e840b94e8094649",
-          type: "small",
+          type: "Standard",
           date: new Date(),
+          source : "Palanikannan1437/incubator-devlake"
         },
         status: "stopped",
+        isGitPodified: false,
       },
       {
-        workspaceId: "6",
+        workspaceId: "palanikanna-rc4conferen-9l0ddzx6jx4",
         ownerId: "string",
         projectId: "Gitpod-6",
         context: {
           branch: "my-branch",
           commit: "f5d4eb4cd3859a760ac613598e840b94e8094649",
-          type: "large",
+          type: "Large",
           date: new Date(),
+          source : "Palanikannan1437/RC4Conferences"
         },
-        status: "stopped",
+        status: "failed",
+        isGitPodified: true,
       },
     ]);
   });
 }
 
-export async function getWorkspace(workspaceId: string): Promise<IWorkspace> {
-  return {
-    workspaceId: workspaceId,
-    ownerId: "string",
-    projectId: "string",
-    context: "Pull request",
-    status: "string",
-  };
-}
+// export async function getWorkspace(workspaceId: string): Promise<IWorkspace> {
+//   return {
+//     workspaceId: workspaceId,
+//     ownerId: "string",
+//     projectId: "string",
+//     context: "Pull request",
+//     status: "string",
+//   };
+// }
 
-export async function startWorkspace(workspaceId: string): Promise<{ instanceId: string; workspaceUrl: string }> {
-  return { instanceId: "1", workspaceUrl: "workspace-1" };
-}
+// export async function startWorkspace(workspaceId: string): Promise<{ instanceId: string; workspaceUrl: string }> {
+//   return { instanceId: "1", workspaceUrl: "workspace-1" };
+// }
 
-export async function stopWorkspace(workspaceId: string): Promise<string> {
-  return `Workspace ${workspaceId} has been stopped`;
-}
+// export async function stopWorkspace(workspaceId: string): Promise<string> {
+//   return `Workspace ${workspaceId} has been stopped`;
+// }

2a05429b03007e95e8fb5a7722c50663b6cce6c5
Henit Chobisa <chobisa.henit@gmail.com>
2023-01-01 23:08:16 +0530

    feat: Workspace Schema Added

---
+++ types/IWorkspace.ts
@@ -0,0 +1,9 @@
+import IWorkspaceContext from "./IWorkspaceContext"
+
+export default interface IWorkspace {
+    workspaceId: string;
+    ownerId: string;
+    projectId: string;
+    context: IWorkspaceContext;
+    status: string;
+}

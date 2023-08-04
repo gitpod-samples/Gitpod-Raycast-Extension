0ebcd4ce4a32232f768ec2ac185325098f4b3082
Henit Chobisa <chobisa.henit@gmail.com>
2023-08-04 17:56:00 +0530

    feat: Added Network Error Safety in Default Org Form

--- src/components/DefaultOrgForm.tsx
+++ src/components/DefaultOrgForm.tsx
@@ -1,8 +1,12 @@
 import { Action, ActionPanel, Detail, Form, LocalStorage, Toast, getPreferenceValues, showToast, useNavigation } from "@raycast/api"
 import { usePromise } from "@raycast/utils"
+import { useState } from "react";
 
+import { IOrganizationError } from "../api/Gitpod/Models/IOrganizationError";
 import { IOrganization } from "../api/Gitpod/Models/IOrganizations"
 import { dashboardPreferences } from "../preferences/dashboard_preferences";
+
+import { ErrorListView, errorMessage } from "./errorListView";
 
 interface defaultOrgParams {
     revalidate?: () => Promise<void>;
@@ -15,10 +19,29 @@ export default function DefaultOrgForm({revalidate} : defaultOrgParams) {
 
     const { pop } = useNavigation();
 
-    const { isLoading, data, error } = usePromise(async () => {
+    const [isUnauthorised, setIsUnauthorized] = useState<boolean>(false);
+    const [isNetworkError, setNetworkError] = useState<boolean>(false);
+    const [data, setData] = useState<IOrganization[]>([])
+
+    const { isLoading , error } = usePromise(async () => {
         const data = await IOrganization.fetchOrganization(preferences.access_token ?? "");
-        return data
+        setData(data)
+    }, [], {
+        onError: (error : Error) => {
+            const e = ( error as any ) as {code: string }
+            if (e.code === "ENOTFOUND"){
+                setNetworkError(true);
+            }
+            const gitpodError = error as IOrganizationError;
+            if (gitpodError.code === 401 || gitpodError.code === 500){
+                setIsUnauthorized(true);
+            }
+        }
     })
+
+    if (isUnauthorised || isNetworkError){
+        return <ErrorListView message={isUnauthorised ? errorMessage.invalidAccessToken : errorMessage.networkError} />
+    }
 
     return (
         error ? <Detail metadata={"Failed to Fetch Organization, Try Again"} /> :
@@ -41,7 +64,8 @@ export default function DefaultOrgForm({revalidate} : defaultOrgParams) {
                 </ActionPanel>
             } >
                 <Form.Dropdown id="default_organization" placeholder="Select Default Organization" title="Default Organization">
-                    {data?.map((org) => <Form.Dropdown.Item title={org.name} value={org.orgId} />)}
+                    { data.length === 0 ? <Form.Dropdown.Item title="Loading Organization" value="" /> :
+                    data?.map((org) => <Form.Dropdown.Item title={org.name} value={org.orgId} />)}
                 </Form.Dropdown>
             </Form>
     )

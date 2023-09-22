e7cddc8aa002fe66fa419865abaab1ecd79849e0
Henit Chobisa <chobisa.henit@gmail.com>
2023-09-22 14:00:12 +0530

    Create from focused context (#71)
    * feat: added helper for getting currently focused context
    
    * feat: added dynamically browser context in menubar
    
    * feat: added focused context helper
    
    * feat: added context view for both browser and hud context
    
    * feat: renamed menubar workspaces

---
+++ src/browser_context.tsx
@@ -0,0 +1,96 @@
+import { LaunchType, List, LocalStorage, getPreferenceValues, launchCommand, useNavigation } from "@raycast/api";
+import { usePromise } from "@raycast/utils";
+import { useEffect, useState } from "react";
+
+import { IWorkspace } from "./api/Gitpod/Models/IWorkspace";
+import { WorkspaceManager } from "./api/Gitpod/WorkspaceManager";
+import DefaultOrgForm from "./components/DefaultOrgForm";
+import View from "./components/View";
+import { ErrorListView, errorMessage } from "./components/errorListView";
+import createWorksapceFromContext from "./helpers/createWorkspaceFromContext";
+import { getFocusedBrowserContext } from "./helpers/getFocusedContext";
+import { dashboardPreferences } from "./preferences/dashboard_preferences";
+import { Preferences } from "./preferences/repository_preferences";
+
+
+const BrowserContext = () => {
+
+    const { push } = useNavigation()
+
+    const [defaultOrganization, setDefaultOrganization] = useState<string | undefined>();
+    const EditorPreferences = getPreferenceValues<Preferences>();
+
+    const dashboardPreferences = getPreferenceValues<dashboardPreferences>();
+
+    if (dashboardPreferences.access_token === undefined || dashboardPreferences.access_token.trim() === "") {
+        return <ErrorListView message={errorMessage.invalidAccessToken} />;
+    }
+
+    const workspaceManager = new WorkspaceManager(dashboardPreferences.access_token ?? "");
+
+    const { isLoading, revalidate } = usePromise(
+        async () => {
+            await workspaceManager.init();
+            const defaultOrg = await LocalStorage.getItem("default_organization");
+            if (defaultOrg !== undefined) {
+                setDefaultOrganization(defaultOrg.toString());
+            }
+        }
+    )
+
+    useEffect(() => {
+        if (!isLoading) {
+            if (!defaultOrganization) {
+                push(<DefaultOrgForm revalidate={revalidate} />)
+            } else {
+                getFocusedBrowserContext().then((context_url) => {
+                    if (context_url) {
+                        console.log(context_url)
+                        IWorkspace.create(WorkspaceManager.api, {
+                            contextUrl: "https://github.com/" + context_url,
+                            organizationId: defaultOrganization.toString(),
+                            ignoreRunningPrebuild: true,
+                            ignoreRunningWorkspaceOnSameCommit: true,
+                            worksspaceClass: EditorPreferences.preferredEditorClass,
+                            ideSetting: {
+                                defaultIde:
+                                    EditorPreferences.preferredEditor === "ssh" ? "code" : EditorPreferences.preferredEditor,
+                                useLatestVersion: false,
+                            },
+                        });
+                        
+  
+                        setTimeout(() => {
+                            launchCommand({
+                                name: "gitpod_dashboard",
+                                type: LaunchType.UserInitiated,
+                            })
+                        }, 2000);
+                    } else {
+                        console.log("Nothing in browser context")
+                    }
+                })
+            }
+        }
+    }, [isLoading])
+
+    return (
+        <List>
+            <List.EmptyView icon={
+                {
+                    source: "Icons/gitpod_logo_bouncing.gif"
+                }
+            } title="Creating Fresh Workspace" />
+        </List>
+    )
+}
+
+
+
+export default function Command() {
+    return (
+        <View>
+            <BrowserContext />
+        </View>
+    );
+}
--- src/helpers/getFocusedContext.ts
+++ src/helpers/getFocusedContext.ts
@@ -1,8 +1,8 @@
 import { runAppleScript } from "run-applescript";
 
-export const getFocusedContext = async () => {
+export const getFocusedBrowserContext = async () => {
     try {
-    const result = await runAppleScript(`
+        const result = await runAppleScript(`
       tell application "System Events"
       set activeApp to name of first application process whose frontmost is true
   end tell
@@ -23,7 +23,7 @@ export const getFocusedContext = async () => {
                   return currentURL
               end if
           end if
-          return "No matching URL"
+          return ""
       end tell
   else if activeApp is "Safari" then
       tell application "Safari"
@@ -46,11 +46,7 @@ export const getFocusedContext = async () => {
   end if
       `)
 
-      if (result){
-        return removeGitHubPrefix(result)
-      }
-
-      return result
+        return result
 
     } catch (e) {
         console.log("error occured")
@@ -58,7 +54,117 @@ export const getFocusedContext = async () => {
     }
 }
 
-function removeGitHubPrefix(url: string): string {
+export const getFocusedTextContext = async () => {
+    const text = await runAppleScript(`
+    -- Back up clipboard contents:
+    set savedClipboard to my fetchStorableClipboard()
+    
+    -- Copy selected text to CLIPBOARD:
+    set thePasteboard to current application's NSPasteboard's generalPasteboard()
+    
+    try
+        -- Copy selected text to clipboard:
+        tell application "System Events" to keystroke "c" using {command down}
+        delay 0.1 -- Allow time for the clipboard to update
+        
+        set theSelectedText to the clipboard
+        
+        if theSelectedText is not "" then
+            -- Makes the selected text all uppercase:
+            set AppleScript's text item delimiters to {quote}
+            set textItems to text items of theSelectedText
+            set AppleScript's text item delimiters to ""
+            set theSelectedText to textItems as text
+            -- From: http://apple.stackexchange.com/a/171196/184907
+            set theModifiedSelectedText to theSelectedText
+            
+            
+            -- Overwrite the old selection with the desired text:
+            set the clipboard to theModifiedSelectedText
+            
+            delay 0.1 -- Without this delay, may restore clipboard before pasting.
+            
+            -- Instead of the above three lines, you could instead use:
+            --      tell application "System Events" to keystroke theModifiedSelectedText
+            -- But this way is a little slower.
+            
+            -- Restore clipboard:
+            my putOnClipboard:savedClipboard
+            
+            return theModifiedSelectedText
+        else
+            return ""
+        end if
+    on error
+        return ""
+    end try
+    
+    use AppleScript version "2.4"
+    use scripting additions
+    use framework "Foundation"
+    use framework "AppKit"
+    
+    
+    on fetchStorableClipboard()
+        set aMutableArray to current application's NSMutableArray's array() -- used to store contents
+        -- get the pasteboard and then its pasteboard items
+        set thePasteboard to current application's NSPasteboard's generalPasteboard()
+        -- loop through pasteboard items
+        repeat with anItem in thePasteboard's pasteboardItems()
+            -- make a new pasteboard item to store existing item's stuff
+            set newPBItem to current application's NSPasteboardItem's alloc()'s init()
+            -- get the types of data stored on the pasteboard item
+            set theTypes to anItem's types()
+            -- for each type, get the corresponding data and store it all in the new pasteboard item
+            repeat with aType in theTypes
+                set theData to (anItem's dataForType:aType)'s mutableCopy()
+                if theData is not missing value then
+                    (newPBItem's setData:theData forType:aType)
+                end if
+            end repeat
+            -- add new pasteboard item to array
+            (aMutableArray's addObject:newPBItem)
+        end repeat
+        return aMutableArray
+    end fetchStorableClipboard
+    
+    
+    on putOnClipboard:theArray
+        -- get pasteboard
+        set thePasteboard to current application's NSPasteboard's generalPasteboard()
+        -- clear it, then write new contents
+        thePasteboard's clearContents()
+        thePasteboard's writeObjects:theArray
+    end putOnClipboard:
+    `)
+
+    return filterText(text);
+}
+
+function filterText(text: string): string {
+    return identifyGitHubEntity(dequote(text.trim()))
+}
+
+
+function dequote(input: string): string {
+    return input.replace(/['"]+/g, '');
+}
+
+
+export function identifyGitHubEntity(url: string): string {
+    const repoPattern = /https?:\/\/github\.com\/[A-Za-z0-9_.-]+\/[A-Za-z0-9_.-]+/;
+    const pullPattern = /https?:\/\/github\.com\/[A-Za-z0-9_.-]+\/[A-Za-z0-9_.-]+\/pull\/\d+/;
+    const issuePattern = /https?:\/\/github\.com\/[A-Za-z0-9_.-]+\/[A-Za-z0-9_.-]+\/issues\/\d+/;
+    const branchPattern = /https?:\/\/github\.com\/[A-Za-z0-9_.-]+\/[A-Za-z0-9_.-]+\/tree\/[A-Za-z0-9_.-]+/;
+
+    if (repoPattern.test(url) || pullPattern.test(url) || issuePattern.test(url) || branchPattern.test(url)) {
+        return url;
+    } else {
+        return '';
+    }
+}
+
+export function removeGitHubPrefix(url: string): string {
     const prefix = 'https://github.com/';
     if (url.startsWith(prefix)) {
         return url.slice(prefix.length);
